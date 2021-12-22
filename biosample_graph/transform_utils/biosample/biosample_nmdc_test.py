b75f36f42de4a5b2021d76b64053a9f96dd129b0
caufieldjh <j.harry.caufield@gmail.com>
2021-12-22 14:38:10 -0500

    Expand Koza transform for NMDC biosamples

--- biosample_graph/transform_utils/biosample/biosample_nmdc_test.py
+++ biosample_graph/transform_utils/biosample/biosample_nmdc_test.py
@@ -1,18 +1,38 @@
 import uuid
 
 from biolink_model_pydantic.model import ( #type: ignore
-    Protein,
+    MaterialSample,
+    MaterialSampleDerivationAssociation,
+    EnvironmentalFeature,
     Predicate
 )
 
 from koza.cli_runner import koza_app #type: ignore
 
 source_name="biosample_nmdc_test"
-full_source_name = "NMCD Biosample Test"
+full_source_name = "NMDC Biosample Test"
 
 row = koza_app.get_row(source_name)
 
 # Entities
+sample = MaterialSample(id='GOLD:' + row['GOLD_ID'],
+                        name= row['BIOSAMPLE_NAME'],
+                        source=full_source_name)
 
+ # ENVO IDs are entities too but we get their metadata elsewhere
 
 # Association
+for envo_level in ['ENV_BROAD_SCALE','ENV_LOCAL_SCALE','ENV_MEDIUM']:
+    if str(row[envo_level]) == '': # No ENVO for this level
+        continue
+    envo = EnvironmentalFeature(id=str(row[envo_level]).replace("_",":"))
+    association = MaterialSampleDerivationAssociation(
+        id="uuid:" + str(uuid.uuid1()),
+        subject=sample.id,
+        predicate=Predicate.derives_from,
+        object=envo.id,
+        relation="RO:0000086", #"has quality" - maybe not the best option
+        source =full_source_name
+    ) 
+
+    koza_app.write(sample, association, envo)
